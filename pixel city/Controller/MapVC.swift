bc5246a605a82785f61fdc78f37535f3c933f11a
Araz Sinjary <araz@Arazs-MBP.Home>
2018-06-29 10:47:46 +0300

    starter project

--- pixel city/Controller/MapVC.swift
+++ pixel city/Controller/MapVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapVC.swift
 //  pixel city
 //
 //  Created by Araz Sinjary on 6/29/18.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
